6591b8c5e083528b8669401765bfd891938a568e
miresque <mirkilinc@gmail.com>
2022-07-22 11:06:31 +0100

    Used useEffect to fetch and render data from API

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import "./styles.css";
 
 export default function App() {
   const [dataType, setDataType] = useState("");
-
   const [data, setData] = useState(null);
 
-  console.log({ data });
-
-  // Write code here.
-  //
+  useEffect(() => {
+    if (dataType) {
+      fetch(`https://swapi.dev/api/${dataType}/`)
+      .then(res => res.json())
+      .then(data => setData(data.results))
+    }
+  }, [dataType])
 
   return (
     <div>
       <SelectTypeForm setDataType={setDataType} />
-      {data && <DataList dataType={dataType} data={data.results} />}
+      {data && <DataList dataType={dataType} data={data} />}
     </div>
   );
 }
--- src/components/SelectTypeForm.js
+++ src/components/SelectTypeForm.js
@@ -1,10 +1,10 @@
 function SelectTypeForm(props) {
-  // Write code here...
+  const { setDataType } = props
 
   return (
     <form className="three-column-grid__expand-two gap-md">
       <label htmlFor="type">Data Type</label>
-      <select id="type" name="type">
+      <select id="type" name="type" onChange={(e) => setDataType(e.target.value)}>
         <option value="">Please select...</option>
         <option value="people">People</option>
         <option value="planets">Planets</option>
